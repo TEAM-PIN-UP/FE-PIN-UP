b336c563cea35056a3ce718623430038d1d0b3d7
Doyoung Park <doyoungp.00@gmail.com>
2024-10-07 18:44:28 +0900

    feat #5 - Add basic map and bottom sheet

--- src/page/Map/index.tsx
+++ src/page/Map/index.tsx
@@ -1,17 +1,42 @@
+import { Sheet } from "react-modal-sheet";
+import {
+  Container as MapDiv,
+  Marker,
+  NaverMap,
+  NavermapsProvider,
+  useNavermaps,
+} from "react-naver-maps";
 import styled from "styled-components";
-import { H1, H2, H3 } from "../../style/font";
 
 const MapPage: React.FC = () => {
+  const naverMaps = useNavermaps();
+
   return (
-    <div>
-      <StTitle>This is a map page</StTitle>
-    </div>
+    <NavermapsProvider ncpClientId={import.meta.env.VITE_NAVER_MAPS}>
+      <StMapDiv>
+        <NaverMap defaultCenter={new naverMaps.LatLng(35.166, 126.908)}>
+          <Marker defaultPosition={new naverMaps.LatLng(35.166, 126.908)} />
+        </NaverMap>
+
+        <Sheet
+          isOpen={true}
+          onClose={() => {}}
+          snapPoints={[0.9, 0.5, 0.1]}
+          initialSnap={1}
+        >
+          <Sheet.Container>
+            <Sheet.Header />
+            <Sheet.Content></Sheet.Content>
+          </Sheet.Container>
+        </Sheet>
+      </StMapDiv>
+    </NavermapsProvider>
   );
 };
 
-const StTitle = styled.div`
-  ${H2};
-  background-color: yellow;
+const StMapDiv = styled(MapDiv)`
+  width: 100vw;
+  height: 100vh;
 `;
 
 export default MapPage;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_NAVER_MAPS: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
