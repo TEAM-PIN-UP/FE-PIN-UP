d017544f4a903e764dbd015707708634f9992edd
Seokwoodang <swoo1429@naver.com>
2024-10-08 15:07:26 +0900

    #7 refactor : edit app, main, router for layout action

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
 import LandingPage from "../page/Landing";
 import MapPage from "../page/Map";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/map",
-    element: <MapPage />,
+    element: <App />, // 최상위 경로로 App 설정
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/map",
+        element: <MapPage />,
+      },
+    ],
   },
 ]);
 
