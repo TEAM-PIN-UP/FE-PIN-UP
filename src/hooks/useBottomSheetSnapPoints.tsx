dfa9f5b8ba4fed628f08b9dfb6198762b6aec5c4
Doyoung Park <doyoungp.00@gmail.com>
2024-10-12 16:42:59 +0900

    Update bottom sheet hook

--- src/hooks/useBottomSheetSnapPoints.tsx
+++ src/hooks/useBottomSheetSnapPoints.tsx
@@ -6,20 +6,25 @@ const useBottomSheetSnapPoints = () => {
   const sheetHeaderRef = useRef<HTMLDivElement>(null);
   const searchHeaderRef = useRef<HTMLDivElement>(null);
 
+  // Calculate bottom sheet minimum snap point at page load
   useEffect(() => {
+    // Use delay to wait until ref elements are rendered
     const timer = setTimeout(() => {
       if (
         attachRef.current &&
         sheetHeaderRef.current &&
         searchHeaderRef.current
       ) {
+        // Get navbar height
         const screenHeight = window.innerHeight;
         const attachHeight = attachRef.current.offsetHeight;
         const navbarHeight = screenHeight - attachHeight;
 
+        // Get sheet header & search header height
         const sheetHeaderHeight = sheetHeaderRef.current.offsetHeight;
         const searchHeaderHeight = searchHeaderRef.current.offsetHeight;
 
+        // Calculate bottom snap position
         const snapPointValue =
           (sheetHeaderHeight + searchHeaderHeight + navbarHeight) /
           screenHeight;
