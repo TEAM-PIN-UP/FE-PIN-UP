893e42ee4fbd4229da5403b8f65319591411f885
Seokwoodang <swoo1429@naver.com>
2024-10-19 21:52:42 +0900

    #28 feat - basic toast function complete

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,26 +1,44 @@
 import styled from "styled-components";
 import alertIcon from "@/image/icons/alertCircleRed.svg";
 import { B4 } from "@/style/font";
+import { useEffect } from "react";
 
 interface ToastProps {
   text: string;
+  toastOn: boolean;
+  setToastOn: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Toast: React.FC<ToastProps> = ({ text }) => {
+interface ToastStyle {
+  toastOn: boolean;
+}
+
+const Toast: React.FC<ToastProps> = ({ text, toastOn, setToastOn }) => {
+  useEffect(() => {
+    if (toastOn) {
+      const timer = setTimeout(() => {
+        setToastOn(false);
+      }, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [toastOn]);
+
   return (
-    <StToast>
+    <StToast toastOn={toastOn}>
       <img src={alertIcon} alt="alert" />
       <span>{text}</span>
     </StToast>
   );
 };
 
-const StToast = styled.div`
+const StToast = styled.div<ToastStyle>`
   position: fixed;
   top: var(--spacing_24);
   left: 50%;
   transform: translate(-50%, 0);
   display: flex;
+  opacity: ${(props) => (props.toastOn ? 1 : 0)};
+  transition: opacity 1s;
   align-items: center;
   width: max-content;
   text-align: left;
