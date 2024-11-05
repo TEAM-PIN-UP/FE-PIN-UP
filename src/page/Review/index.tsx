575cabf8f9b0cdaddcb7c7a0ef25a05ce10c16ad
Seokwoodang <swoo1429@naver.com>
2024-11-05 17:19:01 +0900

    #42 add - placeSearch component add

--- src/page/Review/index.tsx
+++ src/page/Review/index.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import PlaceSearch from "./_components/PlaceSearch/PlaceSearch";
+import ReviewHeader from "./_components/ReviewHeader";
+
 const ReviewPage = () => {
-  return <div>this is ReviewPage</div>;
+  return (
+    <StReview>
+      <ReviewHeader />
+      <PlaceSearch />
+    </StReview>
+  );
 };
+
+const StReview = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  text-align: left;
+`;
 
 export default ReviewPage;
