0f8b0a15b1aa6bfa252dcdc674874914766e595b
Seokwoodang <swoo1429@naver.com>
2024-11-08 15:58:21 +0900

    #42 feat - review write component finish

--- src/page/Review/_components/ReviewTotal/WriteReview.tsx
+++ src/page/Review/_components/ReviewTotal/WriteReview.tsx
@@ -1,25 +1,33 @@
 import { H4 } from "@/style/font";
-import styled from "styled-components";
-import { Editor } from "@toast-ui/react-editor";
 
-const WriteReview = () => {
+import styled from "styled-components";
+import EditorCompo from "./EditorCompo";
+
+export interface WriteReviewProp {
+  reviewContent: string;
+  setReviewContent: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const WriteReview: React.FC<WriteReviewProp> = ({
+  reviewContent,
+  setReviewContent,
+}) => {
   return (
     <StWriteReview>
       <p className="title">리뷰 작성하기</p>
-      <Editor
-        initialValue="hello"
-        previewStyle="vertical"
-        height="600px"
-        // initialEditType="markdown"
-        useCommandShortcut={true}
+      <EditorCompo
+        reviewContent={reviewContent}
+        setReviewContent={setReviewContent}
       />
     </StWriteReview>
   );
 };
 
 const StWriteReview = styled.div`
-  display: grid;
+  display: flex;
+  flex-direction: column;
   gap: 12px;
+  padding: 0 20px;
   .title {
     ${H4}
   }
