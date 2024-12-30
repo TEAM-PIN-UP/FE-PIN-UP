8b060113d775f0e6d5fefc334750be9167e06851
Doyoung Park <doyoungp.00@gmail.com>
2024-12-30 11:53:35 +0900

    Update tos

--- src/page/SignUp/SignUpInterface.ts
+++ src/page/SignUp/SignUpInterface.ts
@@ -1,5 +1,6 @@
 export type AuthProvider = "kakao" | "naver" | "google";
-export type AgreementKey = "tos" | "personalInfo" | "marketing";
+// 순서대로 이용약관 동의, 개인정보 수집, 위치정보 수집, 마케팅 수신
+export type AgreementKey = "TOA" | "TOP" | "TOG" | "TOM";
 
 export interface SignUpForm {
   authMethod: AuthProvider | "";
