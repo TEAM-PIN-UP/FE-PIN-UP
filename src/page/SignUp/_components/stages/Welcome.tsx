add8fb4ff40c9c595e7ed29cc70e1c58d3cfb73c
Doyoung Park <doyoungp.00@gmail.com>
2024-12-30 11:53:21 +0900

    Update welcome screen

--- src/page/SignUp/_components/stages/Welcome.tsx
+++ src/page/SignUp/_components/stages/Welcome.tsx
@@ -1,18 +1,22 @@
 import Button from "@/components/Button";
 import { useNavigate } from "react-router-dom";
-import StGlue from "../typography/StGlue";
 import StTextContainer from "../typography/StTextContainer";
 
 const Welcome = () => {
   const navigate = useNavigate();
   return (
     <>
-      <StTextContainer>
-        <div className="h1">환영합니다!</div>
-        <div className="h1">아래 버튼을 눌러</div>
-        <div className="h1">핀업을 시작해봐요</div>
+      <StTextContainer
+        style={{
+          display: "flex",
+          flexGrow: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className="h1">반가워요!</div>
+        <div className="h1">이제 ‘찐’ 맛집 리뷰를 탐색해볼까요?</div>
       </StTextContainer>
-      <StGlue />
       <Button
         size="full"
         onClick={() => {
