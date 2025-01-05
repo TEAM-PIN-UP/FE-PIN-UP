e526d39110201ae1c4cca2022cba37dea622462e
Doyoung Park <doyoungp.00@gmail.com>
2025-01-05 14:31:59 +0900

    Add types

---
+++ src/types/place.ts
@@ -0,0 +1,24 @@
+// 키워드 없이 장소 목록 조회 인터페이스
+// 리뷰가 있는 장소 목록만 조회 / 카테고리(음식점, 카페), 정렬조건(가까운 순, 최신 순, 별점 높은 순, 별점 낮은 순)
+
+export interface PlaceParams {
+  category?: string;
+  sort?: string;
+  swLatitude: string;
+  swLongitude: string;
+  neLatitude: string;
+  neLongitude: string;
+  currentLatitude: string;
+  currentLongitude: string;
+}
+
+export interface Place {
+  placeId: number;
+  kakaoPlaceId: string;
+  name: string;
+  averageStarRating: number;
+  reviewCount: number;
+  distance: string;
+  reviewImageUrls: string[];
+  reviewerProfileImageUrls: string[];
+}
---
+++ src/types/placeKeyword.ts
@@ -0,0 +1,18 @@
+// 키워드로 장소 목록 조회 인터페이스
+// 리뷰 작성 시 장소 목록 조회할 때 사용 / 카카오맵 API 호출함
+
+export interface PlaceKeywordParams {
+  query: string;
+}
+
+export interface PlaceKeyword {
+  kakaoMapId: string;
+  name: string;
+  category: string;
+  address: string;
+  roadAddress: string;
+  latitude: string;
+  longitude: string;
+  reviewCount: number;
+  averageStarRating: number;
+}
