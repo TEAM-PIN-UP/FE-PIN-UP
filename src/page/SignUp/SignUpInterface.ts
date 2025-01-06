bc1d13ebe559197740ab1c2d568b53d2349d6d1e
Doyoung Park <doyoungp.00@gmail.com>
2025-01-06 14:40:22 +0900

    Add member response type

--- src/page/SignUp/SignUpInterface.ts
+++ src/page/SignUp/SignUpInterface.ts
@@ -1,4 +1,5 @@
 export type AuthProvider = "kakao" | "naver" | "google";
+// 이용약관 동의, 개인정보 수집, 위치정보 수집, 마케팅 수신
 export type AgreementKey = "TOA" | "TOP" | "TOG" | "TOM";
 
 export interface SignUpForm {
@@ -6,4 +7,14 @@ export interface SignUpForm {
   nickname: string;
   profileImage: string;
   agreedToTerms: AgreementKey[];
+}
+
+export interface MemberResponse {
+  memberId: number;
+  email: string;
+  name: string;
+  nickname: string;
+  profilePictureUrl: string;
+  bio: string;
+  termsOfMarketing: "Y" | "N";
 }
