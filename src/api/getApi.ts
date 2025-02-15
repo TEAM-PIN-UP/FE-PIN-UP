f20513cc74babd8ec0426e3832faeac929490fac
Doyoung Park <doyoungp.00@gmail.com>
2025-02-15 14:30:00 +0900

    Remove unused get api

--- src/api/getApi.ts
+++ src/api/getApi.ts
@@ -3,7 +3,6 @@ import {
   GetSearchPlacesRequest,
   GetSpecificPlaceRequest,
 } from "@/interface/apiInterface";
-import { ReviewDetail } from "@/interface/review";
 import customAxios from "./Interceptor";
 
 const getApi = {
@@ -23,17 +22,11 @@ const getApi = {
     ).toString();
     return customAxios.get(`/api/places?${queryParams}`);
   },
-
   getSearchPlaces: ({ keyword }: GetSearchPlacesRequest) =>
     customAxios.get(`/api/places/keyword?query=${keyword}`),
-
   getMemberNicknameCheck: ({ nickname }: { nickname: string }) =>
     customAxios.get(`/api/members/nickname/check?nickname=${nickname}`),
-
   getMyFeed: () => customAxios.get(`/api/members/me/feed`),
-
-  getReviewId: ({ id }: { id: string }) =>
-    customAxios.get<ReviewDetail>(`/api/reviews/${id}`),
   getMyPlace: () => customAxios.get(`/api/bookmarks/my`),
 };
 
