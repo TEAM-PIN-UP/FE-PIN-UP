5c78f5217951ee487badb8faedffac5c135294f0
Doyoung Park <doyoungp.00@gmail.com>
2025-02-19 13:12:34 +0900

    Clear DOM error on GraphBar

--- src/page/Map/_components/review/GraphBar.tsx
+++ src/page/Map/_components/review/GraphBar.tsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 const GraphBar = ({ percentage = 0 }: { percentage: number }) => {
   return (
     <div>
-      <StGraphBar percentage={percentage}>
+      <StGraphBar $percentage={percentage}>
         <div className="fill" />
       </StGraphBar>
     </div>
   );
 };
 
-const StGraphBar = styled.div<{ percentage: number }>`
+const StGraphBar = styled.div<{ $percentage: number }>`
   display: flex;
   width: 6px;
   height: 64px;
@@ -18,7 +18,7 @@ const StGraphBar = styled.div<{ percentage: number }>`
   border-radius: 8px;
   .fill {
     width: 100%;
-    height: calc(64 * ${(props) => props.percentage}px);
+    height: calc(64 * ${(props) => props.$percentage}px);
     background-color: black;
     border-radius: 8px;
     margin-top: auto;
