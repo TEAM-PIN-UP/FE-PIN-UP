2b9b94be13190e624d2eb1950407ded971408f98
Doyoung Park <doyoungp.00@gmail.com>
2025-02-20 21:08:58 +0900

    Add naver auth to env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_NAVER_MAPS: string;
   readonly VITE_SERVER_ADDRESS: string;
   readonly VITE_GOOGLE_AUTH_CLIENT_ID: string;
+  readonly VITE_NAVER_AUTH_CLIENT_ID: string;
+  readonly VITE_NAVER_AUTH_CLIENT_SECRET: string;
   // more env variables...
 }
 
