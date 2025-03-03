306c410061e559b75ef7ebb977773787c98fc424
Doyoung Park <doyoungp.00@gmail.com>
2025-03-03 17:10:05 +0900

    Configure Sentry

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,18 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(), // Put the Sentry vite plugin after all other plugins
+  sentryVitePlugin({
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+    telemetry: false,
 
-    // Put the Sentry vite plugin after all other plugins
-    sentryVitePlugin({
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-      telemetry: false,
-
-      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-  ],
+    // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+  }), sentryVitePlugin({
+    org: "pinup-p6",
+    project: "pinup-web"
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
