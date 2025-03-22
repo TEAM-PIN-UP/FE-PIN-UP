45a27ebdf5782290cd0c12274197274900ccbebe
Doyoung Park <doyoungp.00@gmail.com>
2025-03-22 12:37:57 +0900

    Split profile buttons

---
+++ src/page/Profile/_components/FriendButton.tsx
@@ -0,0 +1,83 @@
+import { useSentFriendRequests } from "@/hooks/api/pinBuddy/useFriendRequests";
+import usePostFriendRequests from "@/hooks/api/pinBuddy/usePostFriendRequest";
+import addUser from "@/image/icons/addUser.svg";
+import { paths } from "@/routes/paths";
+import { ModalProps } from "@/store/modalStore";
+import { B4 } from "@/style/font";
+import useModalPopup from "@/utils/modalPopup";
+import useToastPopup from "@/utils/toastPopup";
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+interface FriendButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  isOtherUser: boolean;
+}
+
+const FriendButton: React.FC<FriendButtonProps> = ({ isOtherUser }) => {
+  const navigate = useNavigate();
+  const { uid: id } = useParams();
+  const { openModal, closeModal } = useModalPopup();
+  const toast = useToastPopup();
+
+  const friendRequest = usePostFriendRequests();
+  const { data: sentFriendRequests } = useSentFriendRequests();
+  const friendRequestModal: ModalProps = {
+    type: "cancel-ok",
+    title: "핀버디를 신청하시겠어요?",
+    body: ["상대가 신청을 수락해야", "핀버디가 맺어져요."],
+    okButtonText: "신청",
+    cancelButtonText: "취소",
+    onOkButtonClick: () => {
+      if (id) {
+        friendRequest.mutate({ receiverId: id });
+        toast("핀버디 신청이 완료되었어요.");
+      } else {
+        console.error("Send friend request failed: no receiver id.");
+        toast("핀버디 신청이 실패했어요.");
+      }
+      closeModal();
+    },
+    onCancelButtonClick: closeModal,
+  };
+  const hasReceived = sentFriendRequests?.some(
+    (request) => String(request.receiver.memberId) === id
+  );
+
+  const handleAddFriend = () => {
+    if (isOtherUser) openModal(friendRequestModal);
+    else navigate(paths.profile.search());
+  };
+
+  return (
+    <StButton onClick={handleAddFriend}>
+      <img src={addUser} />
+      <span>{hasReceived ? "신청 완료" : "핀버디 신청"}</span>
+    </StButton>
+  );
+};
+
+const StButton = styled.button`
+  ${B4}
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  gap: var(--spacing_6);
+  align-items: center;
+  justify-content: center;
+  border: none;
+  border-radius: var(--radius_8);
+  background-color: var(--neutral_100);
+  padding: var(--spacing_12);
+  box-sizing: content-box;
+  height: 16px;
+  cursor: pointer;
+  transition: transform 0.02s ease-in-out, background-color 0.02s ease-in-out;
+
+  &:active {
+    background-color: var(--neutral_200);
+    transform: scale(0.97);
+  }
+`;
+
+export default FriendButton;
